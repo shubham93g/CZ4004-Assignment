0a7966f2b24ceea2f7c46c87f38f2765801c03ce
Shubham Goyal <shubhamgoyal@Shubhams-MacBook-Pro.local>
2014-10-09 20:00:25 +0800

    Initial Commit

---
+++ CZ4004 Assignment/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CZ4004 Assignment
+//
+//  Created by Shubham Goyal on 9/10/14.
+//  Copyright (c) 2014 Shubham Goyal. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
